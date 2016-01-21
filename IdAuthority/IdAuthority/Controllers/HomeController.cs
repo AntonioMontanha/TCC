b8397160c520250814afbbade7d1160466c884ea
cgimenes <celostalker@gmail.com>
2016-01-21 19:59:20 -0200

    Just doing some tests

--- IdAuthority/IdAuthority/Controllers/HomeController.cs
+++ IdAuthority/IdAuthority/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.CodeDom;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using Facebook;
 using Microsoft.AspNet.Facebook;
@@ -9,12 +10,31 @@ namespace IdAuthority.Controllers
 {
     public class HomeController : Controller
     {
+        private const string appID = "706984349411685";
+
         [FacebookAuthorize("email", "user_photos")]
         public async Task<ActionResult> Index(FacebookContext context)
         {
             if (ModelState.IsValid)
             {
                 var user = await context.Client.GetCurrentUserAsync<MyAppUser>();
+
+                var client = new FacebookClient();
+                dynamic result = client.Get("oauth/access_token", new
+                {
+                    client_id = appID,
+                    redirect_uri = @"https://localhost:44300/",
+                    client_secret = "7a0dad24c1dae8c8eaec7fdf2bbec5f0",
+                    grant_type = "client_credentials"
+                    //user_token = "CAAKCZC58CKWUBAKhX7OXkvXYn1X2tZC6UQK4oeZAOAYkEkWZAT7eZAVM1DrzN62heqZBDm4yBMLJe5AywBlGHCFm8gMJGhptEXMMgxZA8cv3PPk99jU9McnMbZBFv0z8IAwskuk6YfOiHTph3tTfvkJa368TZBWQefVfBdgwwdPlDjs7cmqeJXNRyWXJFsBdqWRqDymZABOnZAVewZDZD"
+                });
+
+                client.AccessToken = ((Facebook.JsonObject) (result))["access_token"].ToString();
+                
+
+                dynamic testUsers = client.Get(appID + "/accounts/test-users");
+                dynamic teste = client.Get("140855076293667/feed");
+                return View("Index");
 
                 return View(user);
             }
