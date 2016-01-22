a46b0e66cd3a111bcf371901841fcbd3822ff3a7
cgimenes <celostalker@gmail.com>
2016-01-22 13:01:16 -0200

    Getting test user id

--- IdAuthority/IdAuthority/Controllers/HomeController.cs
+++ IdAuthority/IdAuthority/Controllers/HomeController.cs
@@ -18,6 +18,22 @@ namespace IdAuthority.Controllers
             {
                 var user = await context.Client.GetCurrentUserAsync<MyAppUser>();
 
+
+                var client = new FacebookClient();
+                dynamic result = client.Get("oauth/access_token", new
+                {
+                    client_id = appID,
+                    redirect_uri = @"https://localhost:44300/",
+                    client_secret = "7a0dad24c1dae8c8eaec7fdf2bbec5f0",
+                    grant_type = "client_credentials"
+                    //user_token = "CAAKCZC58CKWUBAKhX7OXkvXYn1X2tZC6UQK4oeZAOAYkEkWZAT7eZAVM1DrzN62heqZBDm4yBMLJe5AywBlGHCFm8gMJGhptEXMMgxZA8cv3PPk99jU9McnMbZBFv0z8IAwskuk6YfOiHTph3tTfvkJa368TZBWQefVfBdgwwdPlDjs7cmqeJXNRyWXJFsBdqWRqDymZABOnZAVewZDZD"
+                });
+
+                client.AccessToken = ((Facebook.JsonObject)(result))["access_token"].ToString(); // Obtendo o token de acesso
+
+                dynamic testUsers = client.Get(appID + "/accounts/test-users"); // Busca os amigos
+                string id = ((JsonObject) ((JsonArray) ((JsonObject) testUsers)["data"])[0])["id"].ToString();
+                dynamic teste = client.Get(id + "/feed"); // Busca os posts
                 
                 return View("Index");
 
